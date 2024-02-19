0409e33f507c17ad5000b6fdce9c44800d9b907a
Dibyendu Das <get2dibyendu@gmail.com>
2024-02-19 11:22:59 +0530

    font changed

--- src/pages/legacy.tsx
+++ src/pages/legacy.tsx
@@ -93,7 +93,7 @@ function legacy() {
 
       <div className="my-28 flex flex-col items-center justify-center">
         <h1
-          className="text-3xl lg:text-7xl my-3 text-white text-center font-[Butler] font-normal lg:text-start lg:-ms-14 z-10"
+          className="text-2xl md:text-4xl lg:text-7xl my-3 text-white text-center font-[Butler] font-normal lg:text-start lg:-ms-14 z-10"
           style={{
             letterSpacing: 5,
             lineHeight: 1.4,
@@ -102,12 +102,12 @@ function legacy() {
           HOUSE OF KASTURI
         </h1>
 
-        <h3 className="text-white text-center lg:text-start lg:-ms-14 z-10 text-3xl font-medium mb-5">
+        <h3 className="text-white text-center lg:text-start lg:-ms-14 z-10 text-xl lg:text-3xl font-medium mb-5">
           The House of Kasturi is the soul of Diamas
         </h3>
 
-        <h3 className="text-white w-[80%]  lg:w-8/12 text-center mb-9 lg:-ms-14 z-10 text-lg ">
-          a place where tradition echoes in every jewel, radiating a richness
+        <h3 className="text-white mx-auto pt-5  text-justify font-normal text-xl leading-tight lg:w-[75%] tracking-wider mt-5 p-10 sm:w-full ">
+          A place where tradition echoes in every jewel, radiating a richness
           that withstands time's test. Aditya's origins in diamond artistry are
           rooted here, under his father Surinder Kumar's visionary guidance.
           With timeless expertise, Surinder helps Aditya craft pieces elegant in
@@ -115,13 +115,14 @@ function legacy() {
           present yet aims for the future - destined to breathe new life into
           enduring legacies.
         </h3>
+
       </div>
 
       <img src={two} alt="two" className="w-full object-cover" />
 
       <div className="my-28 flex flex-col items-center justify-center">
         <h1
-          className="text-3xl lg:text-7xl my-3 font-[Bolter] text-white text-center lg:text-start lg:-ms-14 z-10"
+          className="text-2xl md:text-4xl lg:text-7xl my-3 font-[Bolter] text-white text-center lg:text-start lg:-ms-14 z-10"
           style={{
             letterSpacing: 5,
             lineHeight: 1.4,
@@ -130,7 +131,10 @@ function legacy() {
           SURINDER KUMAR
         </h1>
 
-        <h3 className="text-white w-[80%] lg:w-8/12 text-center lg:-ms-14 z-10 text-lg">
+        <h3 className="text-white mx-auto pt-5  text-justify font-normal text-xl leading-tight lg:w-[75%] tracking-wider mt-5 p-10 sm:w-full ">
+          A place where tradition echoes in every jewel, radiating a richness
+          that withstands time's test. Aditya's origins in diamond artistry are
+          rooted here, under his father Surinder Kumar's visionary guidance.
           Surinder Kumar, the visionary voice behind Diamas, imprints the spirit
           of craftsmanship across generations. Just as his father passed the
           passion for jewellery on to Surinder, Surinder guides his son Aditya.
@@ -143,6 +147,20 @@ function legacy() {
           creations sparkle with his influence radiating through each 
           elegant facet as timeless as the diamonds themselves.
         </h3>
+
+        {/* <h3 className="text-white w-[80%] lg:w-8/12 text-center lg:-ms-14 z-10 text-lg">
+          Surinder Kumar, the visionary voice behind Diamas, imprints the spirit
+          of craftsmanship across generations. Just as his father passed the
+          passion for jewellery on to Surinder, Surinder guides his son Aditya.
+          He inspires Aditya with his eye for perfection and multifaceted
+          leadership. Through respect for heritage and a duty to innovation,
+          Surinder steers Diamas towards a timeless legacy. In every cut and
+          setting, one may glimpse shadows of the man in the style, the
+          attention to detail, the refusal to settle for less than the sublime.
+          Surinder's passion becomes purpose, craft matures as art, and Diamas’
+          creations sparkle with his influence radiating through each 
+          elegant facet as timeless as the diamonds themselves.
+        </h3> */}
       </div>
     </div>
   );
